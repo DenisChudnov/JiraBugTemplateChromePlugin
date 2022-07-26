dfd3d8430d16cf0254e84deb9198f59f7794f781
Denis Chudnov <denis.chudnov@saritasa.com>
2022-07-26 20:08:50 +0700

    Make button open&fill issue card via one click

--- popup.js
+++ popup.js
@@ -1,4 +1,49 @@
-let open_create_window_btn = document.getElementById("show");
+function clickOnCreateButton() {
+        let createButton = document.getElementById('createGlobalItemIconButton');
+        createButton.click();
+
+        const observer = new MutationObserver((mutations, obs) => {
+            const issueTypeDropdownSelector = document.getElementById('issue-create.ui.modal.create-form.type-picker.issue-type-select');
+            if (issueTypeDropdownSelector) {
+                let description_field = document.getElementById('description-container');
+                const description = description_field.querySelector('.ProseMirror');
+                const paragraph = description.querySelectorAll('p');
+                paragraph[0].parentNode.innerHTML="" +
+                    "<p><strong>ISSUE:</strong></p>" +
+                    "<p><strong>STEPS TO REPRODUCE:</strong></p>\n" +
+                    "    <ul className=\"ak-ul\" data-indent-level=\"1\">\n" +
+                    "        <li><p>Open …</p></li>\n" +
+                    "    </ul>" +
+                    " <p><strong>ACTUAL RESULT:</strong></p>" +
+                    "<p><strong>EXPECTED RESULT:</strong></p>";
+                issueTypeDropdownSelector.click();
+                obs.disconnect();
+                return;
+            }
+        })
+
+    observer.observe(document, {
+        childList: true,
+        subtree: true
+    })
+
+}
+
+function fillBugDescriptionWithTemplate() {
+        let description_field = document.getElementById('description-container');
+        const description = description_field.querySelector('.ProseMirror');
+        const paragraph = description.querySelectorAll('p');
+        paragraph[0].parentNode.innerHTML="" +
+            "<p><strong>ISSUE:</strong></p>" +
+            "<p><strong>STEPS TO REPRODUCE:</strong></p>\n" +
+            "    <ul className=\"ak-ul\" data-indent-level=\"1\">\n" +
+            "        <li><p>Open …</p></li>\n" +
+            "    </ul>" +
+            " <p><strong>ACTUAL RESULT:</strong></p>" +
+            "<p><strong>EXPECTED RESULT:</strong></p>";
+}
+
+let open_create_window_btn = document.getElementById("show_and_fill");
 let fill_bug_description_btn = document.getElementById("fill")
 
 open_create_window_btn.addEventListener("click", async () => {
@@ -16,27 +61,5 @@ fill_bug_description_btn.addEventListener("click", async () => {
         function: fillBugDescriptionWithTemplate,
     });
 })
-
-function clickOnCreateButton() {
-        let createButton = document.getElementById('createGlobalItemIconButton');
-        createButton.click();
-}
-
-function fillBugDescriptionWithTemplate() {
-        let description_field = document.getElementById('description-container');
-        const description = description_field.querySelector('.ProseMirror');
-        const paragraph = description.querySelectorAll('p');
-        paragraph[0].parentNode.innerHTML="" +
-            "<p><strong>ISSUE:</strong></p>" +
-            "<p><strong>STEPS TO REPRODUCE:</strong></p>\n" +
-            "    <ul className=\"ak-ul\" data-indent-level=\"1\">\n" +
-            "        <li><p>Open …</p></li>\n" +
-            "    </ul>" +
-            " <p><strong>ACTUAL RESULT:</strong></p>" +
-            "<p><strong>EXPECTED RESULT:</strong></p>";
-}
-
-
-
 
 
